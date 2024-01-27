9a05f4203399e809f6002dfcfcaa8978cadfd09b
Nfalconi27 <nelson_falconi@hotmail.com>
2024-01-26 22:44:21 -0300

    agora é possível selecionar uma foto da galeria e alterar a img principal

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('Você não selecionou nenhuma imagem')
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+        <ImageViewer 
+        placeholderImageSource={PlaceholderImage}
+        selectedImage={selectedImage}
+        />
       </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Escolha uma foto" onPress={pickImageAsync}/>
+        <Button label="Use essa foto!"/>
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -29,5 +53,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
-  }
+  },
+  footerContainer:{
+    flex: 1/3,
+    alignItems: 'center',
+  },
 });
